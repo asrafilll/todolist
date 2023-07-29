614440f229f788fac060f74534a9a62ec86f6ef3
asrafilll <asrafilr@gmail.com>
2023-07-29 14:26:14 +0700

    Todo App done

---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  return (
+    <div className='Todo'>
+        <p onClick={() => toggleComplete(task.id)} className={`${task.isCompleted ? 'completed' : ""}`}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon onClick={()=> editTodo(task.id)} icon={faPenToSquare} />
+        <FontAwesomeIcon onClick={()=> deleteTodo(task.id)} icon={faTrash} />
+        </div>
+    </div>
+  )
+}
